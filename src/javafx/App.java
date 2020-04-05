adc2553a1c37ee5d963b2b9fb0c2dcf5154cbc29
Cabomaldade <soutom@yahoo.com.br>
2020-04-05 20:54:04 -0300

    Modality - Style - Criação Nova Janela

--- src/javafx/App.java
+++ src/javafx/App.java
@@ -1,7 +1,9 @@
 package javafx;
 
 import javafx.application.Application;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class App extends Application {
 
@@ -14,6 +16,41 @@ public class App extends Application {
     public void start(Stage stage) throws Exception {
         stage.setTitle("Cabomaldade Application");
         stage.show();
+
+        // Criando uma nova janela(stage)
+        Stage segundaJanela = new Stage();
+        segundaJanela.setTitle("Segunda Janela");
+        segundaJanela.show();
+
+        // Tamanho da Janela
+        segundaJanela.setWidth(900);
+        segundaJanela.setHeight(600);
+
+        // Posicao da janela na tela
+        segundaJanela.setX(200);
+        segundaJanela.setY(100);
+
+        Stage terceiraJanela = new Stage();
+        terceiraJanela.setTitle("Terceira Janela");
+
+        // Modality
+        // só usa essa janela - outras paradadas até fechar essa
+        // terceiraJanela.initModality(Modality.APPLICATION_MODAL);
+
+        // janela 2 é dona da 3 - só usa segunda quando fecha terceira
+        terceiraJanela.initModality(Modality.WINDOW_MODAL);
+        terceiraJanela.initOwner(segundaJanela);
+
+        //terceiraJanela.initModality(Modality.NONE); // Default
+
+        // Style
+        terceiraJanela.initStyle(StageStyle.DECORATED);
+
+        terceiraJanela.isFullScreen();
+
+        terceiraJanela.show();
+
+
     }
 
     @Override
