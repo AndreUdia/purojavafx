57057f4b197f811152e953f0a8ab040277a690b8
Cabomaldade <soutom@yahoo.com.br>
2020-04-05 19:36:37 -0300

    Primeira Janela

---
+++ src/javafx/Main.java
@@ -0,0 +1,12 @@
+package javafx;
+
+import javafx.application.Application;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Application.launch(App.class, args);
+
+    }
+}
